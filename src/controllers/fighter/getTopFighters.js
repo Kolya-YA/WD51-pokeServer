3951cd39b34783ae28a78fa0b463722bad65ec7f
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2024-07-04 20:37:36 +0200

    chore: Update getTopFighters to sort by score in descending order and limit to 10 fighters

--- src/controllers/fighter/getTopFighters.js
+++ src/controllers/fighter/getTopFighters.js
@@ -2,8 +2,7 @@ import Fighter from "../../models/fighter.js";
 
 const getTopFighters = async (req, res, next) => {
 	try {
-		// TODO: Add conditions to filter top of fighters 
-		const fighters = await Fighter.find({}).select('name fights'); 
+		const fighters = await Fighter.find({}).select('name fights').sort({ 'fights.score': -1 }).limit(10); 
 		res.status(200).json(fighters);
 	} catch (error) {
 		next(error);
