de924d72e800bfb82d3e1ac328de81943fea1136
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2024-07-10 10:05:15 +0200

    chore: Update battleRoutes.js to add authentication middleware to battle routes

--- src/routes/battleRoutes.js
+++ src/routes/battleRoutes.js
@@ -1,15 +1,14 @@
-import express from 'express';
-import getLastBattles from '../controllers/battle/getLastBattles.js';
-import postNewBattle from '../controllers/battle/postNewBattle.js';
-import getButtlesByUserId from '../controllers/battle/getButtlesByUserId.js';
+import express from "express";
+import getLastBattles from "../controllers/battle/getLastBattles.js";
+import postNewBattle from "../controllers/battle/postNewBattle.js";
+import getButtlesByUserId from "../controllers/battle/getButtlesByUserId.js";
+
+import auth from "../middleware/auth.js";
 
 const battleRouter = express.Router();
 
 battleRouter.get("/last", getLastBattles);
-
-battleRouter.get('/:userId', auth, getButtlesByUserId);
-
+battleRouter.get("/:userId", auth, getButtlesByUserId);
 battleRouter.post("/", auth, postNewBattle);
-
 
 export default battleRouter;
